c038883907abaf54925c224cd7fc90e39dac2fa5
subingh <sghatuwal@gmail.com>
2024-04-08 12:28:24 +0545

    added a new functionality

--- HomeController.cs
+++ HomeController.cs
@@ -23,6 +23,11 @@ namespace WebApplication2.Controllers
             return View();
         }
 
+        public IActionResult Fun()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
